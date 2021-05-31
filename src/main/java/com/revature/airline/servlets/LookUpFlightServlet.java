8c7fe49827ecd336efe9466bada363e0b8e525a5
Kyle Plummer <KylePlummerBSCS@gmail.com>
2021-05-30 20:01:21 -0400

    Update LookUpFlightServlet.java

--- src/main/java/com/revature/airline/servlets/LookUpFlightServlet.java
+++ src/main/java/com/revature/airline/servlets/LookUpFlightServlet.java
@@ -15,7 +15,7 @@ public class LookUpFlightServlet extends HttpServlet {
 
     private Dispatcher dispatcher= new Dispatcher();
     @Override
-    protected void doPost(HttpServletRequest req, HttpServletResponse resp) {
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) {
         dispatcher.dataDispatch (req, resp, "lookup");
 
         resp.setStatus(202);
